89dbf88c883b7d8c827d95a2749a3468bf7c2d8e
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-06-10 13:58:21 +1000

    Updated to version 1.3.2

--- peek_doc_user/conf.py
+++ peek_doc_user/conf.py
@@ -25,7 +25,7 @@ title = 'Synerty Peek - User Documentation'
 __project__ = 'SynertyPeek-UserDocs'
 __copyright__ = '2019, Synerty'
 __author__ = 'Synerty'
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 import sphinx_rtd_theme
 
